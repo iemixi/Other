e8bcd2acb2f2a9b1e9d330115513912e80bdd6e0
Filipp Voronov <fivoronov@gmail.com>
2022-08-20 22:24:01 +0300

    Add bonus

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static String[] products = { "Хлеб", "Пачка гречки", "Упаковка яиц", "Мороженка" };
     public static int[] prices = { 50, 135, 65, 53 };
+    public static int MIN_COST_FOR_BONUS = 1000;
 
     // В стоимости этих товаров каждые три товара должны стоить как два:
     public static String[] productsOnSale = { "Хлеб", "Мороженка" };
@@ -37,6 +38,11 @@ public class Main {
 
         System.out.println("Ваша корзина покупок:");
         int sum = 0;
+        int sumForBonus = 0;
+        for (int i = 0; i < products.length; i++) {
+            sumForBonus += prices[i] * counts[i];
+        }
+        boolean doBonus = sumForBonus >= MIN_COST_FOR_BONUS;
         for (int i = 0; i < products.length; i++) {
             if (counts[i] != 0) {
                 boolean isOnSale = false;
@@ -46,13 +52,16 @@ public class Main {
                     }
                 }
 
+                int amount = doBonus ? counts[i] : counts [i];
+                int rubles = counts[i] / 3 * 2 + counts[i] % 3;
                 if (isOnSale) {
-                    System.out.println("\t" + products[i] + " " + counts[i] + " шт. за " + (prices[i] * (counts[i] / 3 * 2 + counts[i] % 3)) + " руб. (распродажа!)");
-                    sum += prices[i] * (counts[i] / 3 * 2 + counts[i] % 3);
+                    System.out.println("\t" + products[i] + " " + (doBonus ? counts[i] + 1 : counts[i]) + " шт. за " + (prices[i] * counts[i]) + " руб. (Распродажа)");
+                    sum += prices[i] * rubles;
                 } else {
-                    System.out.println("\t" + products[i] + " " + counts[i] + " шт. за " + (prices[i] * counts[i]) + " руб.");
+                    System.out.println("\t" + products[i] + " " + amount + " шт. за " + (prices[i] * counts[i]) + " руб.");
                     sum += prices[i] * counts[i];
                 }
+
             }
         }
         System.out.println("Итого: " + sum + " руб.");
